2e3f63e6010f7bc2f4b26b7ead79982ead8feeab
Lemonade <lemonadegt@gmail.com>
2016-12-26 15:10:07 +0900

    12/26 실습

---
+++ Recoder_MC.py
@@ -0,0 +1,34 @@
+import time
+import math
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+pos1 = mc.player.getTilePos()
+x1 = pos1.x
+y1 = pos1.y
+z1 = pos1.z
+time1 = time.clock()
+
+time.sleep(10)
+
+pos2 = mc.player.getTilePos()
+x2 = pos2.x
+y2 = pos2.y
+z2 = pos2.z
+time2 = time.clock()
+
+xDistance = x2 - x1
+yDistance = y2 - y1
+zDistance = z2 - z1
+
+distance = math.sqrt((x2-x1)**2 + (z2-z1)**2)
+timeGap = time2 - time1
+
+positionChatMessage = "The player has moved x:" + str(xDistance) + " y:" + str(yDistance) + " z:" + str(zDistance)
+mc.postToChat(positionChatMessage)
+
+distanceChatMessage = "The player has moved " + str(int(distance)) + " blocks"
+mc.postToChat(distanceChatMessage)
+
+gapChatMessage = "The player spend " + str(int(timeGap)) + "(s) moving time"
+mc.postToChat(gapChatMessage)
---
+++ chkPassword.py
@@ -0,0 +1,12 @@
+password = 'asdjkl123'
+inputpw = input("패스워드를 입력해주세요 :")
+
+while password != inputpw:
+    inputpw = input("패스워드가 틀렸습니다. 다시 입력해주세요 :")
+
+print("로그인 되었습니다")
+
+#3. 3번 틀리면 접속 불가
+#4. 비밀번호 변경, 새 비밀번호로 로그인
+#5. 컴퓨터와 가위바위보 (randint()이용)
+#6. 10회 게임 진행, 승률 계산
